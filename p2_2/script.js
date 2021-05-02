26e771ced1340fcfb286d8afc8cf1065b2f0df85
Faulischlupf <robin.palmer@gmx.de>
2021-05-02 13:34:38 +0200

    2_2 Finished

--- p2_2/script.js
+++ p2_2/script.js
@@ -104,62 +104,105 @@ var twotwo;
         }
         return arraySplitOutput;
     }
-    /*
-
-    Aufgabe 2 - Arrays
-    ⚠️ Für die folgende Aufgabe sollen die übergebenen Arrays nicht verändert werden, sondern das Ergebnis als neues Array zurückgegeben werden.
-    Lesen Sie dazu im Zweifelsfall nochmal den Abschnitt Call by reference / call by value in 2.2 noch einmal durch.
-    
-    Die meisten dieser Funktionen sind in einem Array bereits Standardmäßig enthalten. Sie sollen diese Funktionen aber selbstverständlich selbst implementieren.
-    Gehen Sie von number Arrays aus.
-    
-    b) Schreiben Sie eine Funktion join(...), welche zwei Arrays zusammenfügt indem es das zweite hinter das erste hängt. Bonus: Können Sie diese Funktion so umschreiben,
-    dass Sie beliebig viele Arrays entgegen nimmt?
-    
-    c) Eine Funktion split(...), die ein Array und zwei Indexe entgegen nimmt und das Array zwischen diesen beiden Indexen zurück gibt.
-    Bonus: Hier wäre eine Prüfung der übergebenen Indizes angebracht. Welche Prüfungen sind dies? Implementieren Sie auch das.
-    
-    Testcode
-    
-    Wenn Sie alle diese Aufgaben erledigt haben, können Sie diesen Code zum testen nutzen. Funktioniert alles wie erwartet?
-    
-    let arr: number[] = [5, 42, 17, 2018, -10, 60, -10010];
-    let arrBack: number[] = backwards(arr);
+    /*testcode*/
+    let arr = [5, 42, 17, 2018, -10, 60, -10010];
+    let arrBack = backwards(arr);
     console.log(arr);
     console.log(arrBack);
-    console.log(join(arr, [15, 9001, -440] ));
-    console.log(join([123, 666, -911], arr, [15, 9001, -440, 1024] )); // Bonus b)
+    console.log(join(arr, [15, 9001, -440]));
     arr = split(arr, 0, 4);
     console.log(arr);
     console.log(split(arr, 1, 2));
-    console.log(split(arr, 2, 0));     // Bonus c)
-    console.log(split(arr, -1, 2));    // Bonus c)
-    console.log(split(arr, 0, 7));     // Bonus c)
-    
-    
-    Aufgabe 3 - Endlich was visuelles!
-    Für diese Aufgabe soll der Canvas genutzt werden (siehe Canvas in 2.2).
-    
-    a) Experimentieren Sie ein wenig mit dem Canvas und machen Sie sich damit vertraut. Malen Sie Linien, machen Sie diese Linien farbig oder gekrümmt,
-    malen Sie Kreise, Kurven und Rechtecke und füllen Sie diese mit Farben. Zeichnen Sie damit eine einfache Landschaft (grüner Boden, blauer Himmel mit ein paar Wolken,
-        ein Haus und ein Baum im Bild). Machen Sie Gebrauch von Html5CanvasTutorials.
-    
-    b) Entwerfen Sie ein Interface, welches auf sinnvolle Weise ein beliebiges Rechteck abbilden kann.
-    Bonus: Machen Sie statt einem Interface eine Klasse und, statt globaler Funktionen denen die Rechtecke übergeben werden in den folgenden Teilaufgaben, Methoden der Klasse selbst.
-    
-    c) Schreiben Sie eine Funktion createRect(), welche Ihnen ohne Übergabeparameter ein zufällig (aber sinnvoll) befülltes Rechteck zurück gibt.
-     Für die Klasse wäre dies der Konstruktor.
-    
-    d) Schreiben Sie eine Funktion drawRect(...), welche das ihr übergebene Rechteck auf dem Canvas malt.
-    
-    e) Lassen Sie jedes Mal, wenn die Seite neu geladen wird, einige Rechtecke generieren und zeichnen.
-    Legen Sie die Rechtecke dafür in einem Array an und rufen für jedes im Array vorhandene Rechteck die drawRect Funktion auf.
-    
-    f) Bonus: Nutzen sie die setTimeout Funktion um alle 50 Millisekeunden den Canvas neu zu bemalen (Sie können ihn über context.clearRect() leeren).
-    Damit es auch interessante Änderungen zu sehen gibt, fügen sie den Rechtecken eine Bewegungsrichtung für beide Achsen hinzu und bewegen Sie diese pro Zyklus um diese Werte.
-    
-    g) Bonus: Ändern Sie ihre Rechteck-Klasse so ab, dass diese von einer neuen, Zeichenobjekt Klasse erbt.
-    Lassen Sie außerdem von Zeichenobjekt eine neue Klasse Kreis erben, welche statt einem Rechteck einen Kreis zeichnet.
-    Duplizieren Sie keinen (oder so wenig wie möglich) Code und nutzen Sie die Vererbung von Klassen voll aus.*/
+    /* 2_2_3 a*/
+    let canvas = document.getElementById("myFirstCanvas");
+    let context = canvas.getContext("2d");
+    context.lineWidth = 10;
+    //sky
+    context.strokeStyle = "blue";
+    context.fillStyle = "blue";
+    context.beginPath();
+    context.moveTo(500, 250);
+    context.lineTo(500, 0);
+    context.lineTo(0, 0);
+    context.lineTo(0, 250);
+    context.closePath();
+    context.stroke();
+    context.fill();
+    //grass
+    context.strokeStyle = "green";
+    context.fillStyle = "green";
+    context.beginPath();
+    context.moveTo(0, 400);
+    context.lineTo(500, 400);
+    context.lineTo(500, 250);
+    context.lineTo(0, 250);
+    context.stroke();
+    context.fill();
+    context.closePath();
+    //cloud
+    context.strokeStyle = "white";
+    context.fillStyle = "white";
+    context.beginPath();
+    context.moveTo(20, 50);
+    context.bezierCurveTo(20, 10, 70, 5, 70, 50);
+    context.bezierCurveTo(70, 6, 100, 2, 100, 50);
+    context.closePath();
+    context.stroke();
+    context.fill();
+    //house
+    context.strokeStyle = "white";
+    context.fillStyle = "white";
+    context.beginPath();
+    context.fillRect(75, 140, 150, 110);
+    //door
+    context.fillStyle = "Brown";
+    context.beginPath();
+    context.fillRect(130, 190, 40, 60);
+    //roof
+    context.strokeStyle = "red";
+    context.fillStyle = "red";
+    context.beginPath();
+    context.moveTo(50, 140);
+    context.lineTo(150, 60);
+    context.lineTo(250, 140);
+    context.closePath();
+    context.stroke();
+    context.fill();
+    //tree
+    context.strokeStyle = "brown";
+    context.fillStyle = "brown";
+    context.beginPath();
+    context.fillRect(350, 130, 30, 150);
+    context.strokeStyle = "darkGreen";
+    context.fillStyle = "darkGreen";
+    context.beginPath();
+    let path = new Path2D();
+    path.arc(365, 90, 90, 0, 2 * Math.PI);
+    context.stroke(path);
+    context.fill(path);
+    /*2_2_3 b-e*/
+    class Rectangle {
+        constructor() {
+            this.xLeft = Math.random() * 500;
+            this.yHigh = Math.random() * 400;
+            this.xRight = Math.random() * 500;
+            this.yLow = Math.random() * 400;
+        }
+        drawRect() {
+            context.lineWidth = 5;
+            context.strokeStyle = "Black";
+            context.beginPath();
+            context.moveTo(this.xLeft, this.yHigh);
+            context.lineTo(this.xRight, this.yHigh);
+            context.lineTo(this.xRight, this.yLow);
+            context.lineTo(this.xLeft, this.yLow);
+            context.closePath();
+            context.stroke();
+        }
+    }
+    for (let i = 1; i <= 5; i++) {
+        let rectangle = new Rectangle();
+        rectangle.drawRect();
+    }
 })(twotwo || (twotwo = {}));
 //# sourceMappingURL=script.js.map
