21521f75fa9b18e5c436359bfba52e90b9f0a2a8
Faulischlupf <robin.palmer@gmx.de>
2021-05-14 17:01:20 +0200

    p2_4 rought Finish

--- p2_KA/script.js
+++ p2_KA/script.js
@@ -5,15 +5,49 @@ var p2_KA;
     let h1 = document.createElement("h1");
     h1.innerText = "Der Schwertinator";
     document.body.appendChild(h1);
+    let display = document.createElement("div");
+    display.setAttribute("id", "display");
+    document.body.appendChild(display);
     let flexbox = document.createElement("div");
     flexbox.setAttribute("id", "flexbox");
     document.body.appendChild(flexbox);
-    function buildSword(_parts, index) {
+    function displaySword() {
+        let picture1 = document.createElement("img");
+        picture1.setAttribute("height", 100 + "px");
+        picture1.setAttribute("src", sessionStorage.getItem("picture1"));
+        let picture2 = document.createElement("img");
+        picture2.setAttribute("width", 100 + "px");
+        picture2.setAttribute("src", sessionStorage.getItem("picture2"));
+        let picture3 = document.createElement("img");
+        picture3.setAttribute("height", 300 + "px");
+        picture3.setAttribute("src", sessionStorage.getItem("picture3"));
+        if (getSubpage() == "guard.html") {
+            display.appendChild(picture1);
+        }
+        if (getSubpage() == "blade.html") {
+            display.appendChild(picture1);
+            display.appendChild(picture2);
+        }
+        if (getSubpage() == "sword.html") {
+            display.appendChild(picture1);
+            display.appendChild(picture2);
+            display.appendChild(picture3);
+        }
+    }
+    function buildSword(_parts, _index) {
         let partsBlock = document.createElement("div");
         flexbox.appendChild(partsBlock);
         let picture = document.createElement("img");
         picture.setAttribute("src", _parts.picture);
-        picture.setAttribute("height", 300 + "px");
+        if (_index == 1) {
+            picture.setAttribute("height", 200 + "px");
+        }
+        if (_index == 2) {
+            picture.setAttribute("height", 50 + "px");
+        }
+        if (_index == 3) {
+            picture.setAttribute("height", 300 + "px");
+        }
         partsBlock.appendChild(picture);
         let name = document.createElement("p");
         name.innerText = "Name: " + _parts.name;
@@ -32,35 +66,49 @@ var p2_KA;
         partsBlock.appendChild(buttons);
         buttons.addEventListener("click", handelClickButton);
         function handelClickButton(_event) {
-            sessionStorage.setItem("npicture" + index, _parts.picture);
-            sessionStorage.setItem("name" + index, _parts.name);
-            sessionStorage.setItem("atk" + index, "" + _parts.atk);
-            sessionStorage.setItem("durability" + index, "" + _parts.durability);
-            sessionStorage.setItem("Price" + index, "" + _parts.price);
-            console.log(_parts);
+            sessionStorage.setItem("picture" + _index, _parts.picture);
+            sessionStorage.setItem("name" + _index, _parts.name);
+            sessionStorage.setItem("atk" + _index, "" + _parts.atk);
+            sessionStorage.setItem("durability" + _index, "" + _parts.durability);
+            sessionStorage.setItem("Price" + _index, "" + _parts.price);
+            if (_index == 1) {
+                window.open("../p2_KA/guard.html", "_self");
+            }
+            if (_index == 2) {
+                window.open("../p2_KA/blade.html", "_self");
+            }
+            if (_index == 3) {
+                window.open("../p2_KA/sword.html", "_self");
+            }
         }
     }
-    for (let i = 0; i < sword.hilt.length; i++) {
-        buildSword(sword.hilt[i], 1);
+    function getSubpage() {
+        return window.location.pathname.substring(window.location.pathname.lastIndexOf("/") + 1);
     }
-    for (let i = 0; i < sword.guard.length; i++) {
-        buildSword(sword.guard[i], 2);
+    if ("hilt.html" == getSubpage()) {
+        for (let i = 0; i < sword.hilt.length; i++) {
+            buildSword(sword.hilt[i], 1);
+        }
     }
-    for (let i = 0; i < sword.blade.length; i++) {
-        buildSword(sword.blade[i], 3);
+    if ("guard.html" == getSubpage()) {
+        displaySword();
+        for (let i = 0; i < sword.guard.length; i++) {
+            buildSword(sword.guard[i], 2);
+        }
+    }
+    if ("blade.html" == getSubpage()) {
+        displaySword();
+        for (let i = 0; i < sword.blade.length; i++) {
+            buildSword(sword.blade[i], 3);
+        }
+    }
+    if ("sword.html" == getSubpage()) {
+        displaySword();
     }
 })(p2_KA || (p2_KA = {}));
 /*
-Aufgabe 1
-
-c) Sorgen Sie dafür, dass jede Kategorie auf einer eigenen Seite auswählbar wird.
-Versuchen Sie dabei sich an den “Code kopieren ist schlecht” Grundsatz zu halten (vorallem für den TS Code, weniger für den HTML Code). Dafür bieten sich zwei Vorgehensweisen an:
-Entweder Sie kopieren die HTML Seiten und finden einen Weg, wie sie dem TypeScript miteilen können, welche Seite gerade geöffnet ist / welche Kategorie geladen werden soll oder
-Sie finden eine Möglichkeit, auf der gleichen Seite die drei Unterschiedlichen Kategorien eine nach der anderen, und nur eine auf einmal anzuzeigen.
-
-d) Sofern noch nicht passiert, zeigen Sie auf der Auswahlseite an, welche Teilaspekte bereits in vorherigen Schritten ausgewählt wurden.
-
 Aufgabe 2
 Entwickeln Sie eine weitere Seite, auf die nachdem (je) ein Element aus allen Kategorieren ausgewählt wurde weitergeleitet wird.
-Auf dieser Seite sollte die grafische Darstellung zusammengeführt werden. Nutzen Sie dazu die Daten die Sie im Browser Cache abgelegt haben sowie die Daten aus der daten.ts Datei.*/ 
+Auf dieser Seite sollte die grafische Darstellung zusammengeführt werden. Nutzen Sie dazu die Daten die Sie im Browser Cache abgelegt haben sowie die Daten aus der daten.ts Datei.
+*/ 
 //# sourceMappingURL=script.js.map
